c2b7f54eb4477585a70e94dd516d423bd65d003d
Harpreet Singh <hshssingh4@gmail.com>
2017-12-08 09:19:11 -0600

    added get by id functionality

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ let UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    recipes: {type: [mongoose.Schema.RecipeSchema], default: []},
+    favorites: {type: [mongoose.Schema.RecipeSchema], default: []},
     following: {type: [mongoose.Schema.UserSchema], default: []},
     followers: {type: [mongoose.Schema.UserSchema], default: []}
 });
