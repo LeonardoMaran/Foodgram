4e8b4604c7173aa942dc0ff65d1dc8ea11f0f082
vanessawang2 <vwang@visa.com>
2017-12-12 16:50:52 -0600

    Authentication

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {Input} from 'semantic-ui-react'
+import {Input, Image} from 'semantic-ui-react'
+import axios from 'axios';
 
 import styles from '../../styles/login.css';
 import Modal from '../Modal/Modal.jsx'
@@ -40,32 +41,67 @@ export class Login extends Component {
 
 
 	handleLogin(event) {
-		this.props.handler(this.state.username);
+		axios.post('http://localhost:3000/api/auth/login', {
+			  username: this.state.username,
+			  password: this.state.password
+			})
+			.then(function(response) {
+				// Log response
+				console.log(response.data.message);
+
+				this.props.handler(this.state.username);
+
+			}.bind(this))
+			.catch(function(error) {
+				// Log response
+				console.log(error);
+			});
+	}
+
+	handleRegister(event) {
+		if (this.state.password === this.state.verify) {
+			axios.post('http://localhost:4000/api/auth/register', {
+			  name: this.state.name,
+			  username: this.state.username,
+			  password: this.state.password
+			})
+			.then(function(response) {
+
+				console.log(response.data.message);
+
+				this.props.handler(this.state.username);
+
+			}.bind(this))
+			.catch(function(error) {
+				// Log response
+				console.log(error);
+			});
+		}
 	}
 
     render() {
         return(
             <div className="Login">
-                <h1>Log In</h1>
                 <div className="Main">
+                	<h1>Foodgram</h1>
 	                <div className="ui input"><Input type="text" value={this.state.username} onChange={this.handleUpdateUser} placeholder="Username"/></div><br/>
-	                <div className="ui input"><Input type="text" value={this.state.password} onChange={this.handleUpdatePassword} placeholder="Password"/></div><br/>
-	                <button className="ui primary button" onClick = {this.handleLogin}>Log In</button>
+	                <div className="ui input"><Input type="password" value={this.state.password} onChange={this.handleUpdatePassword} placeholder="Password"/></div><br/>
+	                <button className="LoginButton ui primary button" onClick = {this.handleLogin.bind(this)}>Log In</button>
                 </div>
 
                 <div className="Register">
                 	<p>Need an account?</p>
-                	<button className="ui primary button" onClick={this.toggleModal}>Sign Up</button>
+                	<button className="RegisterButton ui primary button" onClick={this.toggleModal}>Sign Up</button>
                 </div>
 
                 <Modal show={this.state.modalOpen} onClose={this.toggleModal}>
                 	<div className="Modal">
-                	<div className="ui input"><input type="text" value={this.state.name} onChange={this.handleUpdateName} placeholder="Full Name"/></div><br/>
-	                <div className="ui input"><input type="text" value={this.state.username} onChange={this.handleUpdateUser} placeholder="Username"/></div><br/>
-	                <div className="ui input"><input type="text"  value={this.state.password} onChange={this.handleUpdatePassword} placeholder="Password"/></div><br/>
-	                <div className="ui input"><input type="text" value={this.state.password} onChange={this.handleUpdateVerify} placeholder="Verify Password"/></div><br/>
-	                <button className="ui primary button" onClick = {this.handleLogin}>Sign Up</button>
-                </div>
+	                	<div className="ui input"><input type="text" value={this.state.name} onChange={this.handleUpdateName} placeholder="Full Name"/></div><br/>
+		                <div className="ui input"><input type="text" value={this.state.username} onChange={this.handleUpdateUser} placeholder="Username"/></div><br/>
+		                <div className="ui input"><input type="password"  value={this.state.password} onChange={this.handleUpdatePassword} placeholder="Password"/></div><br/>
+		                <div className="ui input"><input type="password" value={this.state.verify} onChange={this.handleUpdateVerify} placeholder="Verify Password"/></div><br/>
+		                <button className="SignupButton ui primary button" onClick = {this.handleRegister.bind(this)}>Sign Up</button>
+                	</div>
                 </Modal>
 
             </div>
--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -21,22 +21,38 @@ class Modal extends Component {
     const modalStyle = {
       backgroundColor: '#fff',
       borderRadius: 5,
+      minWidth: 400,
       maxWidth: 500,
       minHeight: 300,
       margin: '0 auto',
-      padding: 30
+      padding: 50,
+      paddingTop: 70
     };
+
 
     // footer
     const footer = {
-      textAlign: 'right'
-    }
+      textAlign: 'right',
+      position: 'fixed',
+      top: 10,
+      right: 10
+    };
+
+
+    //button
+    const close = {
+      borderRadius: 100,
+      cursor: 'pointer',
+      color: '#fff',
+      backgroundColor: '#0E6EB8',
+      borderColor: '#0E6EB8'
+    };
 
     return (
       <div className="Container" style={backdropStyle}>
         <div className="Main" style={modalStyle}>
           <div className="footer" style={footer}>
-              <button onClick={this.props.onClose}>
+              <button onClick={this.props.onClose} style={close}>
                 x
               </button>
           </div>
