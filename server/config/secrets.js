6c02fd8e5b819a45f378575c6782094c7987ae57
gizmo126 <samghuang@users.noreply.github.com>
2018-03-07 03:48:56 -0600

    testing branch

--- server/config/secrets.js
+++
@@ -1,8 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb://sghuang2:password@ds015879.mlab.com:15879/foodgram",
-    secret: "supersecret"
-};
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * Connect all of your endpoints together here.
- */
-module.exports = function (app, router) {
-    app.use('/api', require('./home.js')(router));
-    app.use('/api/users', require('./users.js')(router));
-    app.use('/api/recipes', require('./recipes.js')(router));
-    app.use('/api/auth', require('./auth.js')(router));
-};
