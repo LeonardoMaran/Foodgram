e00ac91fe7c2a7b5e89fa4b73b9106a21d11eb2c
gizmo126 <samghuang@users.noreply.github.com>
2018-03-09 02:10:07 -0600

    added mongo

--- config/secrets.js
+++ config/secrets.js
@@ -5,7 +5,10 @@
  */
 module.exports = {
     token: "secret-starter-mern",
-    mongo_connection: "mongodb://sghuang2:password@ds015879.mlab.com:15879/foodgram",
+    mongo_connection: "mongodb://admin:password>@ds239968.mlab.com:39968/heroku_6lv8nrr6",
     secret: "supersecret"
 };
 //# sourceMappingURL=secrets.js.map
+//#
+//# mongodb://admin:password>@ds239968.mlab.com:39968/heroku_6lv8nrr6
+//# mongodb://sghuang2:password@ds015879.mlab.com:15879/foodgram
